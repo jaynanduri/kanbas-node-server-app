817c1240175bf95f3eb5ca24bc91ba10c4655697
jaynanduri <jayantha.nanduri@gmail.com>
2023-11-20 19:40:02 -0500

    Server component for Lab and Kanbas Page

---
+++ app.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import session from "express-session";
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+import ModuleRoutes from "./modules/routes.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
