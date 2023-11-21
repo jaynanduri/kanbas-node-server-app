145e0bdd3d1d77176972d527f67d28004b85cc57
jaynanduri <jayantha.nanduri@gmail.com>
2023-11-20 20:06:23 -0500

    Changes in database index to resolve errors

--- Database/index.js
+++ Database/index.js
@@ -1,10 +1,9 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-import enrollment from "./enrollment.json";
-import grades from "./grades.json";
-import users from "./users.json";
-import {json} from "express";
+import courses from "./courses.json" assert {type: "json"};
+import modules from "./modules.json" assert {type: "json"};
+import assignments from "./assignments.json" assert {type: "json"};
+import enrollment from "./enrollment.json" assert {type: "json"};
+import grades from "./grades.json" assert {type: "json"};
+import users from "./users.json" assert {type: "json"};
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     courses,
